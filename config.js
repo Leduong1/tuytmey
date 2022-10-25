8a71c171757bc255e3547507cda1c4168a77e5dd
Leduong1 <99656475+Leduong1@users.noreply.github.com>
2022-10-25 09:01:26 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100026977340934';
+const eventName = 'GỬI TẶNG TUYẾT MAI😘😘🥰🥰🥰';
+const titleCard = 'Tặng TUÝT MEY';
+const contentCard = 'Chúc bạn 25/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'mai.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
